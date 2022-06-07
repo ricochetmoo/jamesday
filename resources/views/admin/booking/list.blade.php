f79b7632ef00b7a8b872b5ded27de9b89986d7c4
James Barber <james@jamesbarber.tech>
2022-06-07 12:29:24 +0100

    Added details spoiler

--- resources/views/admin/booking/list.blade.php
+++ resources/views/admin/booking/list.blade.php
@@ -21,6 +21,7 @@
 		<table class="table-auto mx-auto">
 			<thead>
 				<tr>
+					<th class="px-4 py-2 text-black text-left border-b-4 border-indigo-700"></th>
 					<th class="px-4 py-2 text-black text-left border-b-4 border-indigo-700">First Name</th>
 					<th class="px-4 py-2 text-black text-left border-b-4 border-indigo-700">Last Name</th>
 					<th class="px-4 py-2 text-black text-left border-b-4 border-indigo-700">Email</th>
@@ -32,6 +33,7 @@
 			<tbody>
 				@foreach($users as $user)
 				<tr class="@if ($user->booked_on) even:bg-indigo-100 @else bg-red-200 @endif">
+					<td class="px-4 py-2 text-black text-left cursor-grab detailsTrigger" data-target="#details-{{$user->id}}">&#9654;</td>
 					<td class="px-4 py-2 text-black text-left">{{$user->first_name}}</td>
 					<td class="px-4 py-2 text-black text-left">{{$user->last_name}}</td>
 					<td class="px-4 py-2 text-black text-left">{{$user->email}}</td>
@@ -39,9 +41,40 @@
 					<td class="px-4 py-2 text-black text-left">@if($user->can_host) Yes ({{$user->can_host}}) @else No @endif</td>
 					<td class="px-4 py-2 text-black text-left">{{$user->plus_ones->count()}}</td>
 				</tr>
+				<tr class="hidden" id="details-{{$user->id}}">
+					<td colspan="7">
+						@if ($user->can_host)
+						<h2 class="text-lg font-bold mt-1">Hosting Notes</h2>
+						<p class="mt-1">{{$user->hosting_details}}</p>
+						@endif
+						<h2 class="text-lg font-bold mt-1">Other Details</h2>
+						<p class="mt-1">Needs accom? @if ($user->needs_accom) Yes @else No @endif &nbsp;&nbsp; Needs parking? @if ($user->needs_parking) Yes @else No @endif &nbsp;&nbsp; Spoons? @if ($user->spoons_interest) Yes @else No @endif &nbsp;&nbsp; Tour? @if ($user->tour_interest) Yes @else No @endif &nbsp;&nbsp; Escape Room? @if ($user->escape_room_interest) Yes @else No @endif
+					</td>
+				</tr>
 				@endforeach
 			</tbody>
 		</table>
 	</div>
+
+	<script type="text/javascript">
+		function toggle(e)
+		{
+			let element = e.target;
+			let target = document.querySelector(element.getAttribute("data-target"));
+
+			target.classList.toggle("hidden");
+			
+			if (element.innerHTML == "▶")
+			{
+				element.innerHTML = "&#9660;";
+			}
+			else
+			{
+				element.innerHTML = "&#9654;"
+			}
+		}
+
+		window.onload = document.querySelectorAll(".detailsTrigger").forEach((element) => {element.addEventListener("click", (e) => {toggle(e);})});
+	</script>
 
 </x-app-layout>
