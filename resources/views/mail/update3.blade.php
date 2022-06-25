304e8d74f2ed40ae30491e08c1e0d0c21484842f
James Barber <james@jamesbarber.tech>
2022-06-25 12:55:51 +0100

    Tried card thing

---
+++ resources/views/mail/update3.blade.php
@@ -0,0 +1,17 @@
+<body>
+	<h1 class="text-align:center;font-size:2.5em;font-weight:bold">Thanks for coming to Jamesday</h1>
+
+	<p>Hey {{$first_name}},</p>
+
+	<p>Firstly, a huge thank you to everyone who came to the event and made it such a memorable night. I hope you all enjoyed it as much as I did.</p>
+
+	<p>If you didn't notice, Bee spent a lot of the evening taking photos which are now available on <a href="https://photos.app.goo.gl/5Cn8k525qkCwnxPN8" target="_blank">The Jamesday Celebrations 2022 Google Photos Shared Album</a>, which you can add your own photos too as well. Thank you Bee for taking these and editing them so quickly!</p>
+
+	<p>Finally, I did promise some very exclusive Jamesday badges and you can now let me know if you'd like one of these on the Jamesday website (they're free, with postage available at cost). You'll need to let me know by <b>Wednesday 29/06/2022 23:59</b> if you want one. You can log in to the website (once, you'll need to generate a new link at <a href="https://day.jamesbarber.tech/" target="_blank">day.jamesbarber.tech</a> any other times) using the link below:</p>
+
+	<p>You can log in to the system using this magic link: <a href="{{url('/')}}/auth/token/{{$token}}">{{url('/')}}/auth/token/{{$token}}</a></p>
+
+	<p>Cheers,<br>James</p>
+
+	<p><small>You have been sent this email as you are registered for Jamesday 2022.</small></p>
+</body>
